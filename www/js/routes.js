568a7a92f26c100192aa559df772cd27a4d47d24
RafaelLimaReis <rafaellimareis01@gmail.com>
2017-03-21 01:15:20 -0300

    Organize School nova versão

--- www/js/routes.js
+++ www/js/routes.js
@@ -1,9 +1,66 @@
-angular.module('routes',[]).config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
+angular.module('starter')
+.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
+
+    .state('menu',{
+      url: '/menu',
+      templateUrl: 'templates/menu.html',
+      abstract: true
+    })
+
     .state('login', {
       url: '/login',
-      templateUrl: 'templates/login.html'
-    });
+      templateUrl: 'templates/login.html',
+      controller: 'userController'
+    })
+
+    .state('menu.home',{
+      url:'/home',
+      views :{
+        'menuConteudo' : {
+          templateUrl: 'templates/home.html',
+          controller: 'homeController'
+        }
+      }
+    })
+
+    .state('menu.disciplinas',{
+      url:'/disciplinas',
+      views: {
+        'menuConteudo' : {
+          templateUrl: 'templates/disciplinas.html',
+          controller: 'disciplinasController'
+        }
+      }
+    })
+
+    .state('menu.cadDisciplina',{
+      url:'/cadDisciplina',
+      views: {
+        'menuConteudo' : {
+          templateUrl: 'templates/cadDisciplina.html',
+          controller: 'disciplinasController'
+        }
+      }
+    })
+
+    .state('menu.horarios',{
+      url:'/horarios',
+      views: {
+        'menuConteudo' : {
+          templateUrl: 'templates/horarios.html',
+        }
+      }
+    })
+
+    .state('menu.config',{
+      url:'/config',
+      views: {
+        'menuConteudo' : {
+          templateUrl: 'templates/config.html'
+        }
+      }
+    })
+
     $urlRouterProvider.otherwise('/login');
-  });
+  })
